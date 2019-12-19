03089d361f451de67c600b97d6a6becada7f7af8
shawn <shawnpatel196@gmail.com>
2019-12-19 14:28:41 -0600

    finished some goals

--- index.js
+++ index.js
@@ -41,7 +41,22 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age){
+    this.stomach = [];
+    this.name= name,
+    this.age= age
+  }
+  eat(food){
+    if (this.stomach.length < 10){
+      this.stomach.push(food);
+    }
+  }
+  poop(){
+    this.stomach=[];
+  }
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
 }
 
 /*
@@ -59,7 +74,25 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon){
+    this.model = model,
+    this.milesPerGallon = milesPerGallon,
+    this.tank = 0,
+    this.odometer= 0
+  }
+  fill(gallons){
+    this.tank = this.tank+gallons;
+  }
+  drive(distance){
+    if(this.tank * this.milesPerGallon > distance){
+      this.odometer = this.odometer +distance;
+      this.tank=this.tank-distance/this.milesPerGallon;
+    }else{
+      this.odometer=this.tank*this.milesPerGallon;
+      this.tank=0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+  }
 }
 
 /*
@@ -75,6 +108,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+  constructor(attributes){
+    this.name= attributes.name,
+    this.age= attributes.age,
+    this.location= attributes.location
+  }
+  speak(){
+    return `Hello my name is ${this.name}, and I from ${this.location}.`
+  }
 
 }
 
@@ -92,9 +133,29 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian {
+  constructor(attributes) {
+    super(attributes)
+      this.specialty= attributes.specialty,
+      this.favLanguage= attributes.favLanguage
+      this.catchPhrase= attributes.catchPhrase
+     }
+     demo(subject){
+       this.subject = subject;
+       return `Today we are learning about ${subject}`
+     }
+     grade(student, subject){
+       this.subject= subject,
+       this.name= student.name
+       
+       return `${this.student} got a 100 on${this.subject}`
+     }
 
-}
+     
+
+  }
+
+
 
 /*
   TASK 5
